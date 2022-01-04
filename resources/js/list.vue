b61562897dde8549b07c1e1906a3179768e20e96
Jamtech Dev <jamtechba@gmail.com>
2022-01-04 17:17:54 +0530

    Create list.vue

---
+++ resources/js/list.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="widget">
+    <div v-if="activeItems && activeItems.length > 0">
+      <ul>
+        <li v-for="item in activeItems" :key="item.id">
+          {{item.name}}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      loading: true,
+      list: [{name:'John',active: true},
+        {name: 'Jane', active: true},
+        {name: 'Bob', active: false}
+      ],
+    }
+  },
+  computed: {
+    activeItems() {
+      /*
+        return `this.list` as an Array of object and filter by if active is true
+        Expected output
+        [
+          {name: 'John', active: true},
+          {name: 'Jane', active: true}
+        ]
+      */
+return this.list.filter((d)=> d.active === true)
+    }
+  }
+}
+</script>
